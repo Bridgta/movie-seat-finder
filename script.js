51a2c31e109605bc6f923ced7c0e947ededae1f9
Bridget Asser <bridgetasser@gmail.com>
2020-03-10 23:45:41 -0400

    save data to local storage

--- script.js
+++ script.js
@@ -4,16 +4,36 @@ const cout = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
+//save ut up
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+//update on e
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem("seletedSeats", JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//film select
+movieSelect.addEventListener("change", e => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+});
+
+//seat e
 container.addEventListener("click", e => {
     if (
         e.target.classList.contains("seat") &&
